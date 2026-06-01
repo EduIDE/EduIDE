9a3352c64375a0d8bca9cdd883bb784746264102
CodeByNikolas <Nikolas.hack@yahoo.com>
2026-06-01 11:49:46 +0000

    feat: switch Theia bundling to esbuild

---
+++ applications/browser/esbuild.mjs
@@ -0,0 +1,54 @@
+/**
+ * This file can be edited to adjust the esbuild process.
+ * To reset, delete this file and rerun theia build again.
+ */
+import { browserOptions, watch, __dirname, join } from './gen-esbuild.browser.mjs';
+import { nodeOptions } from './gen-esbuild.node.mjs';
+import { copy } from 'esbuild-plugin-copy';
+import fs from 'node:fs';
+import path from 'node:path';
+
+import esbuild from 'esbuild';
+
+browserOptions.plugins.push(
+    copy({
+        assets: [{
+            from: join(__dirname, 'ico', '**', '*'),
+            to: join(__dirname, 'lib', 'frontend')
+        }]
+    }),
+    {
+        name: 'favicon-link',
+        setup(build) {
+            build.onEnd(() => {
+                const indexPath = path.join(__dirname, 'lib', 'frontend', 'index.html');
+                if (fs.existsSync(indexPath)) {
+                    let html = fs.readFileSync(indexPath, 'utf8');
+                    if (!html.includes('rel="icon"')) {
+                        html = html.replace('</head>', '  <link rel="icon" type="image/x-icon" href="./favicon.ico">\n</head>');
+                        fs.writeFileSync(indexPath, html);
+                    }
+                }
+            });
+        }
+    }
+);
+
+const browserContext = await esbuild.context(browserOptions);
+const nodeContext = await esbuild.context(nodeOptions);
+
+if (watch) {
+    await Promise.all([
+        browserContext.watch(),
+        nodeContext.watch()
+    ]);
+} else {
+    try {
+        await browserContext.rebuild();
+        await browserContext.dispose();
+        await nodeContext.rebuild();
+        await nodeContext.dispose();
+    } catch {
+        process.exit(1);
+    }
+}
